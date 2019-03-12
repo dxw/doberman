fee52d9cbf148f60b45d469d85d7343ffdd2e28e
F <f@dxw.com>
2019-03-12 14:05:19 +0000

    Add many more repair issue types

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,5 +1,5 @@
 class Repair < ApplicationRecord
   belongs_to :property
 
-  enum issue_type: [ :other, :no_electricity, :no_gas ]
+  enum issue_type: [ :other, :no_electricity, :no_gas, :no_heating, :no_water, :smell_gas, :water_leak, :water_leak_electrics, :not_secure_access, :exposed_wiring, :alarm_beeping ]
 end
