b863a8fbc5242896acdacaaefafbe86388307a5f
F <f@dxw.com>
2019-03-12 11:09:00 +0000

    Give context appropriate suggestions

--- app/controllers/triage_controller.rb
+++ app/controllers/triage_controller.rb
@@ -3,6 +3,11 @@ class TriageController < ApplicationController
   end
 
   def offer_help
+    @help_content = case params[:issue_type]
+      when 'no_electricity' then 'Do something to fix the electricity!'
+      when 'no_gas' then 'Do something about the gas.'
+      else 'No help here...'
+    end
   end
 
   def end
