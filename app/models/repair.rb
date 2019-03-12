5faa5a43d0dd73faaf45441f7677a88e2b0ba779
F <f@dxw.com>
2019-03-12 16:12:18 +0000

    Add a status field to track repair progress

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -2,4 +2,5 @@ class Repair < ApplicationRecord
   belongs_to :property
 
   enum issue_type: [ :other, :no_electricity, :no_gas, :no_heating, :no_water, :smell_gas, :water_leak, :water_leak_electrics, :not_secure_access, :exposed_wiring, :alarm_beeping ]
+  enum status: [ :reported, :in_progress, :done ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_170811) do
+ActiveRecord::Schema.define(version: 2019_03_12_152941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_170811) do
     t.datetime "updated_at", null: false
     t.bigint "property_id"
     t.text "description"
+    t.integer "status", default: 0
     t.index ["property_id"], name: "index_repairs_on_property_id"
   end
 
