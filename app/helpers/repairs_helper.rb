89f3f6619d87f4da5be657ae73c77264b62ec036
F <f@dxw.com>
2019-03-12 13:12:57 +0000

    Sort issue types to ensure other is always last

--- app/helpers/repairs_helper.rb
+++ app/helpers/repairs_helper.rb
@@ -1,2 +1,13 @@
 module RepairsHelper
+  def sorted_issue_types
+    Repair.issue_types.sort do |a, b|
+      if a[0] == 'other'
+        1
+      elsif b[0] == 'other'
+        -1
+      else
+        a <=> b
+      end
+    end
+  end
 end
