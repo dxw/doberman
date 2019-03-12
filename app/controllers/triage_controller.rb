a7315a8b303f6ef417307e0668683f72465d300c
F <f@dxw.com>
2019-03-12 15:10:12 +0000

    Redirect directly to new repair if we have no solution

--- app/controllers/triage_controller.rb
+++ app/controllers/triage_controller.rb
@@ -4,16 +4,25 @@ class TriageController < ApplicationController
 
   def offer_help
     @issue_type = params[:issue_type]
-    @help_content = case @issue_type
-      when 'no_electricity' then 'Do something to fix the electricity!'
-      when 'no_gas' then 'Do something about the gas.'
-      else 'No help here...'
+    case @issue_type
+      when 'no_electricity'
+        @help_content =  'Do something to fix the electricity!'
+      when 'no_gas'
+        @help_content = 'Do something about the gas.'
+      else
+        redirect_to_repair @issue_type
     end
   end
 
   def end
     if params[:no]
-      redirect_to new_repair_path(repair: { issue_type: params[:issue_type] })
+      redirect_to_repair params[:issue_type]
     end
+  end
+
+  private
+
+  def redirect_to_repair(issue_type)
+    redirect_to new_repair_path(repair: { issue_type: issue_type })
   end
 end
