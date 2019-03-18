4d11225e808a3f4a73db1dde816f95b0242acebb
F <f@dxw.com>
2019-03-18 12:07:19 +0000

    Support repair edits

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -11,6 +11,10 @@ class RepairsController < ApplicationController
     @repair = Repair.new(repair_params)
   end
 
+  def edit
+    @repair = Repair.find(params[:id])
+  end
+
   def create
     @repair = Repair.new(repair_params)
 
@@ -18,6 +22,16 @@ class RepairsController < ApplicationController
       redirect_to @repair
     else
       redirect_to action: :new, repair: repair_params
+    end
+  end
+
+  def update
+    @repair = Repair.find(params[:id])
+
+    if @repair.update(repair_params)
+      redirect_to @repair
+    else
+      redirect_to action: :edit, id: @repair.id
     end
   end
 
